de6039e4adb75d4ccf54e9c95924b164c98e01bd
Juan Franco Sanchez <francosanchez.15@teleperformance.co>
2022-04-18 17:29:50 -0500

    Cors Solutions.

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ app = FastAPI(
 )
 
 origins = [
-    "http://localhost:8080",
     "http://localhost:8000",
     "http://localhost:3000",
-    "http://localhost:8000"
+    "http://127.0.0.1:3000",
+    'http://127.0.0.1:8000'
 ]
 
 app.add_middleware(

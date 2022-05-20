a98786fb2ba18b7d9ad69c70888a11474aeccf04
Juan Pablo <juanpanime@gmail.com>
2022-05-20 12:00:21 -0500

    Timezone changes.

--- config/db.py
+++ config/db.py
@@ -9,7 +9,7 @@ dbName = os.getenv('DB_NAME')
 dbUser = os.getenv('DB_USER')
 dbPassw = os.getenv('DB_PASS')
 
-engine = create_engine(f'mysql+pymysql://{dbUser}:{dbPassw}@{dbHost}/{dbName}')
+engine = create_engine(f'mysql+pymysql://{dbUser}:{dbPassw}@{dbHost}/{dbName}', connect_args={"init_command": "SET SESSION time_zone='-05:00'"})
 
 meta = MetaData()
 conn = engine.connect()
